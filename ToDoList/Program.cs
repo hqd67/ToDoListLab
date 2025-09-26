ce83ee36c0a3bb2fc8fec35e1418bea2bffad8d8
hqd <artemcherepanov19@bk.ru>
2025-09-26 19:55:37 +1000

    Инициализация проекта

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,17 +1,25 @@
-namespace ToDoList
+using System;
+using System.Windows.Forms;
+
+namespace TodoApp
 {
-    internal static class Program
+    static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
+        }
+    }
+
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.Text = "TodoApp";
+            this.Width = 800;
+            this.Height = 600;
         }
     }
 }
